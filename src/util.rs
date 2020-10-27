8d31aa8998c815d4e33f8d21282dede2b4ccd427
SeongChan Lee <foriequal@gmail.com>
2020-10-27 14:41:46 +0900

    Block wheel event from Bluetooth using LowLevelMouseProc

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 #[macro_export]
 macro_rules! c_try {
     ($expr:expr) => {
@@ -68,4 +70,29 @@ macro_rules! c_try_ne {
     ($x: expr, $expr:expr) => {
         unsafe { c_try_ne_unsafe!($x, $expr) }
     };
+}
+
+pub struct ForceSendSync<T>(T);
+
+unsafe impl<T> Send for ForceSendSync<T> {}
+unsafe impl<T> Sync for ForceSendSync<T> {}
+
+impl<T> ForceSendSync<T> {
+    pub fn new(value: T) -> Self {
+        Self(value)
+    }
+}
+
+impl<T> Deref for ForceSendSync<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for ForceSendSync<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
 }
